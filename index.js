7086008ac4e9c362f183c3d140729b966f026d6f
Farah alsoqi <farahmanna97@gmail.com>
2023-10-01 14:12:53 +0300

    Complete Assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// code your solution here
+// code your solution her
+function saturdayFun(String="roller-skate"){
+ return `This Saturday, I want to ${String}!`;
+}
+
+const mondayWork = function ( activity='go to the office' ){
+ let result = `This Monday, I will ${activity}.`;
+ return result;
+}
+mondayWork();
+mondayWork('work from home');
+
+function wrapAdjective(farah='*'){
+          return function(String="special"){
+            return `You are ${farah}${String}${farah}!`;
+          }
+}
+console.log( wrapAdjective("%")("a dedicated programmer")); 
